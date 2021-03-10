8cdb1d7a0c7667a6d44cb0dbfa7dff3377f8caec
alexcrist <alexecrist@gmail.com>
2021-03-09 20:51:42 -0800

    Add main.test.js / main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+// ┌─────────────────┐
+// │ Your code here! │	
+// └─────────────────┘
+
+
+
+// ┌─────────────────────────────────────┐
+// │ Do not modify code below this line. │
+// └─────────────────────────────────────┘
+
+if (typeof ageToAbilities === 'undefined') {
+    ageToAbilities = undefined;
+}
+if (typeof oddIndices === 'undefined') {
+    oddIndices = undefined;
+}
+if (typeof numOddValues === 'undefined') {
+    numOddValues = undefined;
+}
+if (typeof averageStringLength === 'undefined') {
+    averageStringLength = undefined;
+}
+if (typeof firstPunctuationIndex === 'undefined') {
+    firstPunctuationIndex = undefined;
+}
+if (typeof getPlace === 'undefined') {
+    getPlace = undefined;
+}
+
+module.exports = {
+    ageToAbilities,
+    oddIndices,
+    numOddValues,
+    averageStringLength,
+    firstPunctuationIndex,
+    getPlace
+};
